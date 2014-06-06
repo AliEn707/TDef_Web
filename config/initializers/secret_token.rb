f493dfe4054b84ac5b3b2ad98c21dbfd58653acc
AliEn707 <1alien707@qip.ru>
2014-06-07 00:32:38 +0400

    start up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TDefWeb::Application.config.secret_key_base = 'a977ae88644cdda9cc3427f6415325f69e9def6c4bf8b363dadc0fb2b33664cda221d3c3c0511739f6d40730e2bd2b5103b5d38cb5b33788e53736597e7fb220'
