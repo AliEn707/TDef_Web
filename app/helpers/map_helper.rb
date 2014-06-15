8d5f65abdecea352c1322d9b1317b4e8d76499ff
AliEn707 <1alien707@qip.ru>
2014-06-15 12:40:54 +0400

    some fix
    build, walk and see maps must be edited separate

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -13,10 +13,20 @@ module MapHelper
 		</div>'
 		o.html_safe
 	end
+	
+	def mark(name,checked)
+		o= '<div class="buildmark">
+			<input class="hidden" type="checkbox" value="None" id="'+name+'" name="'+name+(checked==true ? '" checked' : '"')+'/>
+			<label for="'+name+'"></label>
+		</div>'
+		o.html_safe
+	end
+	
+	
 	def node(name,w,b)
 		walkmark(name+'w',w)+buildmark(name+'b',b)
 	end
-	def grid(size=20)
+	def grid(size=10)
 		nl='
 		'
 		a='<style type="text/css">
@@ -62,9 +72,12 @@ module MapHelper
 			l.times {a+="<td></td>"+nl}
 			n.times do |j|
 				m=j+1
-				name=((i+1)>size ? ((m+i+1-size)*size-((i+1)-(m+i+1-size))) : (m*size-(n-m)))
-				a+='<td colspan="2">'+
-					node(name.to_s,true,true)+
+				name=((i+1)>size ? ((m+i+1-size)*size-((i+1)-(m+i+1-size))) : (m*size-(n-m)))-1
+				a+='<td colspan="2" '+
+					#'bgcolor=#ff0000'+
+					'>'+
+				#	node((name-1).to_s,true,true)+
+					mark(name.to_s,true)+
 					'</td>
 					'
 			end
