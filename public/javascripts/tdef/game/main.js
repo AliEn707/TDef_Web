da22843485616b29851664e1c2d71e54b61b22a6
osuser <osuser@localhost.localdomain>
2015-02-11 14:55:55 +0300

    Black rectangle

---
+++ public/javascripts/tdef/game/main.js
@@ -0,0 +1,14 @@
+﻿
+var canavas = document.getElementById('mainGameCanvas')
+var canvasParent = canavas.parentNode
+
+canavas.width = canvasParent.offsetWidth - 1
+canavas.height = window.innerHeight - canvasParent.offsetTop - 5
+var ctx = canavas.getContext('2d')
+ctx.fillRect(0,0,canavas.width,canavas.height)
+
+window.onresize = function() {
+	canavas.width = canvasParent.offsetWidth - 1
+	canavas.height = window.innerHeight - canvasParent.offsetTop - 5
+	ctx.fillRect(0,0,canavas.width,canavas.height)
+}
