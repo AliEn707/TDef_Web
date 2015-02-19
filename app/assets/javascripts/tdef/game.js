5936e2f97c01dad9da54343f2a9c56f689d58353
AliEn707 <1alien707@qip.ru>
2015-02-19 12:57:56 +0400

    hotfix

---
+++ app/assets/javascripts/tdef/game.js
@@ -0,0 +1,17 @@
+﻿function addWeelHendler(elem,onWheel){
+	if (elem.addEventListener) {
+		if ('onwheel' in document) {
+			// IE9+, FF17+
+			elem.addEventListener ("wheel", onWheel, false);
+		} else 
+			if ('onmousewheel' in document) {
+				// устаревший вариант события
+				elem.addEventListener ("mousewheel", onWheel, false);
+			} else {
+				// 3.5 <= Firefox < 17, более старое событие DOMMouseScroll пропустим
+				elem.addEventListener ("MozMousePixelScroll", onWheel, false);
+			}
+	}else { // IE<9
+		elem.attachEvent ("onmousewheel", onWheel);
+	}
+}
