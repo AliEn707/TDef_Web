a2e92e0843360b79910ca281790556e2f5bfd924
AliEn707 <1alien707@qip.ru>
2015-05-07 14:01:09 +0400

    hotfix

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ export RAILS_ENV=production
 
 #git reset --hard 
 git pull
-bundle install --path .bundle
+#bundle install --path .bundle
 bundle update --path .bundle
 bundle clean --path .bundle
-rake db:migrate db:seed
-rake assets:precompile
+bundle exec rake db:migrate db:seed
+bundle exec rake assets:precompile
 #add check for error and stop server if 
 rails_server restart
