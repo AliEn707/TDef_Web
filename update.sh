aa8af4215782e4e7bc2092e6c20aacb84932802f
AliEn707 <1alien707@qip.ru>
2015-05-07 14:12:04 +0400

    yet another fix

--- update.sh
+++ update.sh
@@ -3,9 +3,9 @@ export RAILS_ENV=production
 
 #git reset --hard 
 git pull
-#bundle install --path .bundle
-bundle update --path .bundle
-bundle clean --path .bundle
+bundle install --path .bundle
+bundle update 
+bundle clean 
 bundle exec rake db:migrate db:seed
 bundle exec rake assets:precompile
 #add check for error and stop server if 
