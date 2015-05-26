bdc228bd04cac398d8e6ece2286caa1a4186fd9a
AliEn707 <1alien707@qip.ru>
2015-05-26 13:08:25 +0400

    install fix

--- install_rails_ubuntu.sh
+++ install_rails_ubuntu.sh
@@ -4,11 +4,17 @@ sudo rm /var/lib/apt/lists/* -vf
 sudo apt-get clean 
 sudo apt-get autoremove 
 sudo apt-get update
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+
 sudo apt-get upgrade && sudo apt-get install curl htop screen gcc g++ git-core phoronix-test-suite nodejs libgd2-xpm-dev postgresql-client autossh build-essential sudo libpq-dev
 
 touch ~/.rvmrc
 echo -e "export rvm_max_time_flag=20\nCFLAGS=\"-march=native -O3 -ffast-math -pipe -fomit-frame-pointer\"\nrvm_configure_env=(CFLAGS=\"-march=native -O3 -ffast-math -pipe -fomit-frame-pointer\")" >> ~/.rvmrc
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -L https://get.rvm.io | bash -s stable && source ~/.rvm/scripts/rvm && rvm requirements && rvm install ruby rvm --patch railsexpress -n railsexpress && rvm use ruby-railsexpress --default && rvm cleanup all 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -L https://get.rvm.io | bash -s stable && source ~/.rvm/scripts/rvm && rvm requirements && rvm install ruby --patch railsexpress -n railsexpress && rvm use ruby-railsexpress --default && rvm cleanup all 
 
 gem install bundler && bundle config --global --jobs 6
 bundle install --path .bundle
