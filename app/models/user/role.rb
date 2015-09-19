ef9001976390f309c559cfab245e5c1d3af17a4b
AliEn707 <1alien707@qip.ru>
2015-09-19 18:03:52 +0300

    migration fixes

--- app/models/user/role.rb
+++ app/models/user/role.rb
@@ -1,4 +1,12 @@
-class User::Role < ActiveRecord::Base
+class User::Role < ActiveRecord::Base #permissions for user
 	belongs_to :user, touch: true
-	serialize :accept, JSON
+	serialize :accept, Oj
+	
+	after_initialize :set_defaults, :if => :new_record?
+	
+	private
+	
+	def set_defaults
+		self.accept['forum']=true
+	end
 end
--- db/migrate/20150724082926_create_user_roles.rb
+++ db/migrate/20150724082926_create_user_roles.rb
@@ -6,6 +6,8 @@ class CreateUserRoles < ActiveRecord::Migration
 	
       t.timestamps
     end
+      add_index :user_roles, :id, :unique => true
       add_index :user_roles, :user_id
+      execute "CREATE INDEX  index_user_roles_on_acept ON user_roles USING gist (acept gist_trgm_ops);" rescue 0
   end
 end
