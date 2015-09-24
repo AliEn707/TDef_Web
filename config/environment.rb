581ea722e4fc89a9645ff0598046953add8cafab
AliEn707 <1alien707@qip.ru>
2015-09-24 12:02:26 +0300

    inline dir win fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ENV['INLINEDIR'] =  "/tmp"  # for RubyInline
+ENV['INLINEDIR'] =  ENV["TMP"] || "/tmp"  # for RubyInline
 
 # Initialize the Rails application.
 TDefWeb::Application.initialize!
