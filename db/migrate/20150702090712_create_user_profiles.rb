0eb4d0792456f0612621b3299067d93e5f6f648f
AliEn707 <1alien707@qip.ru>
2015-09-25 13:44:07 +0300

    profile index set to gin

--- db/migrate/20150702090712_create_user_profiles.rb
+++ db/migrate/20150702090712_create_user_profiles.rb
@@ -8,8 +8,9 @@ class CreateUserProfiles < ActiveRecord::Migration
     end
     add_index :user_profiles, :user_id
     if (connection.adapter_name.downcase=="postgresql") then
-#      execute "CREATE extension pg_trgm;" 
-      execute "CREATE INDEX  index_user_profiles_on_properties ON user_profiles USING gist (properties gist_trgm_ops);"
+#      execute "CREATE EXTENSION pg_trgm;" 
+#      execute "CREATE EXTENSION btree_gin;" 
+      execute "CREATE INDEX  index_user_profiles_on_properties ON user_profiles USING gin (properties gin_trgm_ops);"
     end
   end
 end
