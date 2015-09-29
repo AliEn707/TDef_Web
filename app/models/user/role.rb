c8fa27984092b242e6d65fe58ea2ece11392c8a2
AliEn707 <1alien707@qip.ru>
2015-09-29 12:34:19 +0400

    JSON serializer instade of Oj
    If used Oj, JSON as fast as Oj

--- app/models/user/role.rb
+++ app/models/user/role.rb
@@ -1,6 +1,6 @@
 class User::Role < ActiveRecord::Base #permissions for user
 	belongs_to :user, touch: true
-	serialize :accept, Oj
+	serialize :accept, JSON
 	
 	after_initialize :set_defaults, :if => :new_record?
 	
