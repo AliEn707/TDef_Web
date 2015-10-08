c851fd65450a41f33edbebad1afde71be9354725
AliEn707 <1alien707@qip.ru>
2015-10-08 15:16:21 +0300

    java next step

--- work/connector/java/connector.java
+++ work/connector/java/connector.java
@@ -1,46 +1,42 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.applet.Applet;
 import netscape.javascript.*;
  
-public class TestApplet extends JApplet {
+public class connector extends Applet {
  
     private JButton button = new JButton("Call Javascript");
     private JLabel label = new JLabel();
  
-    public void init() {
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(button, BorderLayout.NORTH);
-        getContentPane().add(label, BorderLayout.SOUTH);
- 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                Thread runner = new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            testLiveConnect();
-                        } catch (JSException jse) {
-                            // Error
-                            jse.printStackTrace();
-                        }
-                    }
-                });
-                runner.start();
-            }
-        });
+    public void start() {
+        try {
+            JSObject window = JSObject.getWindow(this);
+            // invoke JavaScript function
+            Number age = (Number) window.eval("alert(22)");
+            window.call("logJS",new Object[] {"1",2});
+
+        } catch (JSException jse) {
+            jse.printStackTrace();
+        }
     }
+    
+    public int test() throws JSException {
+      return (int)(Math.random()*2000000);
+    }
+    
     private void testLiveConnect() throws JSException {
         JSObject jso = JSObject.getWindow(this);
  
         // call Javascript's method foo() with no argument
-        String result = (String) jso.call("foo", null);
+        String result = (String) jso.call("foo");
         label.setText(result);
  
         // delay 2 seconds to see the result
         try { Thread.sleep(2000); } catch (InterruptedException ie) {};
  
         // call Javascript's method foo() with two arguments
-        result = (String) jso.call("bar", new String[] {"Alice", "Alisa"});
+        result = (String) jso.call("bar", (Object[])new String[] {"Alice", "Alisa"});
         label.setText(result);
         try { Thread.sleep(2000); } catch (InterruptedException ie) {};
  
